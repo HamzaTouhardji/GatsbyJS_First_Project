efb4e261121b5de77b2d96c815dc68c247ab7919
HamzaTouhardji <h.touhardji@gmail.com>
2021-08-27 13:58:49 +0200

    modification avec le gastby build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,22 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from '../pages/components/Layout'
+import styles from '../pages/styles/home.module.css'
 
 export default function Home() {
   return (
     <Layout>
       <section>
         <div>
-          <h2>Design</h2>
+          <h2>Desissgn</h2>
           <h3>Develop and Deploy</h3>
           <p>Mollit esse cillum culpa excepteur aliquip fugiat minim reprehenderit.</p>
           <p>Mollit esse cillum culpa excepteur aliquip fugiat minim reprehenderit.</p>
           <p>Mollit esse cillum culpa excepteur aliquip fugiat minim reprehenderit.</p>
           <p>Mollit esse cillum culpa excepteur aliquip fugiat minim reprehenderit.</p>
           <p>Mollit esse cillum culpa excepteur aliquip fugiat minim reprehenderit.</p>
+          le bouton
+          <Link className={styles.btn} to="/about">About</Link>
         </div>
       </section>
     </Layout>
